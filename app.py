230fcf11e79189035d662d48817be4d7755d89ab
ccedric99 <cedricblei@outlook.de>
2026-02-28 14:27:05 +0100

    Erste Version

--- app.py
+++ app.py
@@ -87,4 +87,4 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    socketio.run(app, host="0.0.0.0", port=10000, allow_unsafe_werkzeug=True)
